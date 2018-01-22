14cb6d7cb0626e217b716dd5e96619580bff7427
Asus <Asus@DESKTOP-1PBARCG>
2018-01-22 11:29:28 +0600

    initial project has been created

---
+++ src/main/java/com/asha/name_SE_JAVA/buyandsave/BuyandsaveApplication.java
@@ -0,0 +1,12 @@
+package com.asha.name_SE_JAVA.buyandsave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyandsaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyandsaveApplication.class, args);
+	}
+}
